f6f923100db0c377baeb22e741a38cc51cf7d12b
Nilay Sharma <nilaysharma642@gmail.com>
2017-08-28 15:33:08 +0530

    Create JavaRegex3.java

---
+++ JavaRegex3.java
@@ -0,0 +1,8 @@
+
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     regex = "^[A-ZA-z]\\w{7,29}$
+     */
+    public static final String regularExpression = "^[a-zA-Z][\\w]{7,29}$";
+}
